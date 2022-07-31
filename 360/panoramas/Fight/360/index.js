74113c5523f952a50eb4769571a0662fa0579104
moyaimoment <51049688+moyaimoment@users.noreply.github.com>
2022-07-31 15:00:50 -0400

    Add files via upload

---
+++ 360/panoramas/Fight/360/index.js
@@ -0,0 +1,36 @@
+import "https://cdn.jsdelivr.net/npm/uevent@2/browser.js";
+import "https://cdn.jsdelivr.net/npm/three/build/three.min.js";
+import "https://cdn.jsdelivr.net/npm/photo-sphere-viewer@4/dist/photo-sphere-viewer.js";
+import "https://cdn.jsdelivr.net/npm/photo-sphere-viewer@4/dist/plugins/compass.js";
+
+new PhotoSphereViewer.Viewer({
+  panorama: 'https://funneproject.github.io/360/img/panorama/Fight.jpg',
+  container: 'photosphere',
+  caption: 'Fight!',
+  loadingImg: '/360/img/photosphere-logo.gif',
+  loadingTxt: 'Loading!',
+  defaultLat: -0.2,
+  defaultLong: 99.9,
+  autorotateLat: -1,
+  defaultZoomLvl: 80,
+  lang: {
+    autorotate: 'Automatic rotation',
+    zoom      : 'Zoom',
+    zoomOut   : 'Zoom out',
+    zoomIn    : 'Zoom in',
+    move      : 'Move',
+    download  : 'Download',
+    fullscreen: 'Fullscreen',
+    menu      : 'Menu',
+    twoFingers: 'Use two fingers to navigate',
+    ctrlZoom  : 'Use ctrl + scroll to zoom the image',
+    loadError : 'The panorama can\'t be loaded',
+},
+  plugins: [
+    [PhotoSphereViewer.CompassPlugin, {
+      hotspots: [
+        { longitude: '327deg' },
+      ],
+    }],
+  ],
+});
