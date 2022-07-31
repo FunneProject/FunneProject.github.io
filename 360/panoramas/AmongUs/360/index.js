82f76955b55eb8f11f98eec83bb194fa80bd2d52
moyaimoment <51049688+moyaimoment@users.noreply.github.com>
2022-07-31 12:23:58 -0400

    Add files via upload

---
+++ 360/panoramas/AmongUs/360/index.js
@@ -0,0 +1,34 @@
+new PhotoSphereViewer.Viewer({
+  panorama: '/360/img/panorama/AmongUs.jpg',
+  container: 'photosphere',
+  caption: 'Among Us',
+  loadingImg: '/360/img/photosphere-logo.gif',
+  loadingTxt: 'Loading!',
+  defaultLat: 0,
+  defaultLong: 1,
+  autorotateLat: 0,
+  lang: {
+    autorotate: 'Automatic rotation',
+    zoom      : 'Zoom',
+    zoomOut   : 'Zoom out',
+    zoomIn    : 'Zoom in',
+    move      : 'Move',
+    download  : 'Download',
+    fullscreen: 'Fullscreen',
+    menu      : 'Menu',
+    twoFingers: 'Use two fingers to navigate',
+    ctrlZoom  : 'Use ctrl + scroll to zoom the image',
+    loadError : 'The panorama can\'t be loaded',
+},
+  plugins: [
+    [PhotoSphereViewer.CompassPlugin, {
+      hotspots: [
+        { longitude: '0deg' },
+        { longitude: '90deg' },
+        { longitude: '180deg' },
+        { longitude: '270deg' },
+      ],
+    }],
+  ],
+});
+
